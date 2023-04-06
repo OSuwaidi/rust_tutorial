fb1c156cd65f0a5b7731debaea2d4a6026fb22a1
Omar <omar.ali.alsuwaidi@gmail.com>
2023-04-06 11:40:43 +0400

    Improved for performance and conciseness

--- general/examples/timing.rs
+++ general/examples/timing.rs
@@ -2,91 +2,84 @@
 
 use std::time::Instant;
 
-fn measure_time(n_loops: usize, fun: fn(u128) -> u128, n: u128) -> f64 {
+fn measure_time(n_loops: usize, fun: fn(u64) -> u64, n: u64) -> f64 {
     let mut times = vec![0; n_loops];
     for i in 0..n_loops {
         let start = Instant::now();
         fun(n);
         times[i] = start.elapsed().as_nanos()
     }
-    let sum: u128 = times.iter().sum(); // summing using "loop4" would've been faster, but this is more concise
+    let sum: u128 = times.iter().sum(); // summing using "loop1" would've been faster, but this is more concise
     (sum as f64) / (n_loops as f64)
 }
 
-fn loop1(size: u128) -> u128 {
-    let vec: Vec<u128> = (0..=size).collect();
+fn loop1(size: u64) -> u64 {
+    let vec: Vec<u64> = (0..=size).collect();
     let mut sum = 0;
-    for v in vec {
-        sum += v
+    for v in vec { // ownership of elements in "vec" is moved into the loop
+        sum += v // adds values directly
     }
     sum
 }
 
-fn loop2(size: u128) -> u128 {
-    let vec: Vec<u128> = (0..=size).collect();
+fn loop2(size: u64) -> u64 {
+    let vec: Vec<u64> = (0..=size).collect();
+    let mut sum = 0;
+    for v in vec { // ownership of elements in "vec" is moved into the loop
+        sum += &v // adds values indirectly via references (extra step)
+    }
+    sum
+}
+
+fn loop3(size: u64) -> u64 {
+    let vec: Vec<u64> = (0..=size).collect();
+    let mut sum = 0;
+    for &v in &vec { // dereferences the referenced vector "vec"
+        sum += v // adds values directly
+    }
+    sum
+}
+
+fn loop4(size: u64) -> u64 {
+    let vec: Vec<u64> = (0..=size).collect();
     let mut sum = 0;
     for v in &vec {
-        sum += v
+        sum += v // adds values indirectly via references (extra step)
     }
     sum
 }
 
-fn loop3(size: u128) -> u128 {
-    let vec: Vec<u128> = (0..=size).collect();
+fn loop5(size: u64) -> u64 {
+    let vec: Vec<u64> = (0..=size).collect();
     let mut sum = 0;
-    for v in vec {
-        sum += &v
-    }
-    sum
-}
-
-fn loop4(size: u128) -> u128 {
-    let vec: Vec<u128> = (0..=size).collect();
-    let mut sum = 0;
-    for &v in &vec {
-        sum += v
-    }
-    sum
-}
-
-fn loop5(size: u128) -> u128 {
-    let vec: Vec<u128> = (0..=size).collect();
-    let mut sum = 0;
-    for &v in &vec {
-        sum += &v
+    for &v in vec.iter() { // "iter()" returns a reference of "vec", then values are being dereferenced
+        sum += v // adds values directly
     }
     sum
 }
 
 /*
-loop1(): This function iterates over the vector by value, meaning that each element is copied during iteration, which is less efficient than using references.
+Note: Adding values directly is faster than adding references of values!
 
-loop2(): This function iterates over the vector by reference, avoiding copying the elements. However, the sum is updated using a reference.
-
-loop3(): This function iterates over the vector by value, but adds the value by reference in the loop. This is less efficient than iterating by reference.
-
-loop4(): This function iterates over the vector by reference and *dereferences* the reference inside the loop. This is the most efficient way to iterate over the vector and update the sum.
-
-loop5(): This function iterates over the vector by reference but updates the sum using a reference, which is less efficient than loop4().
-
-*** loop4 > loop5 > loop1 > loop2 > loop3
-
-Adding values directly is faster than adding references of values!
+*** Performance (the higher the better): ***
+    loop1 > loop2 > loop3 = loop5 > loop4
 */
 
 fn main() {
-    let time1 = measure_time(1000, loop1, 100000);
+    let number_of_loops: usize = 500;
+    let vector_size: u64 = 1000000;
+    let time1 = measure_time(number_of_loops, loop1, vector_size);
     println!("Took {:?} nanoseconds for loop1", time1);
 
-    let time2 = measure_time(1000, loop2, 100000);
+    let time2 = measure_time(number_of_loops, loop2, vector_size);
     println!("Took {:?} nanoseconds for loop2", time2);
 
-    let time3 = measure_time(1000, loop3, 100000);
+    let time3 = measure_time(number_of_loops, loop3, vector_size);
     println!("Took {:?} nanoseconds for loop3", time3);
 
-    let time4 = measure_time(1000, loop4, 100000);
+    let time4 = measure_time(number_of_loops, loop4, vector_size);
     println!("Took {:?} nanoseconds for loop4", time4);
 
-    let time5 = measure_time(1000, loop5, 100000);
+    let time5 = measure_time(number_of_loops, loop5, vector_size);
     println!("Took {:?} nanoseconds for loop5", time5);
 }
