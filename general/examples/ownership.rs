b2f93bc976a6fe3be5f1e30e39620065f024510e
Omar <omar.ali.alsuwaidi@gmail.com>
2023-04-06 15:10:59 +0400

    Revised and elaborated content

--- general/examples/ownership.rs
+++ general/examples/ownership.rs
@@ -17,35 +17,50 @@ fn main() -> Result<(), Error> {
     return Ok(()); // Return "Ok" variant of success type "()" --> unit value (no meaningful value to return on success)
 }
 
-/* In rust, every bit of memory has exactly ONE owner only, and as soon as the owner gets deleted
-or is out of scope, its memory is immediately freed */
+/*
+In rust, every bit of memory has exactly ONE owner only, and as soon as the owner gets deleted
+or is out of scope, its memory is immediately freed
+*/
 
-/* Additionally, when you pass a variable to a function/method/loop or assign it to another variable,
+/*
+Additionally, when you pass a variable to a function/method/loop or assign it to another variable,
 the default behavior is to *move* ownership of that variable into the function/method/loop or the new variable,
-and as such, the old variable will no longer be accessible since its ownership of that piece of memory has *moved* */
+and as such, the old variable will no longer be accessible since its ownership of that piece of memory has *moved*
+*/
 
-/* Thus, we had to pass a mutable ***reference*** (&) to "n" instead of "n" directly into the "read_line()" method,
+/*
+Thus, we had to pass a mutable ***reference*** (&) to "n" instead of "n" directly into the "read_line()" method,
 because otherwise, the ownership of "n" would be ***moved*** to the "read_line()" method, and then we would
-not be able to access the "n" variable after the method call */
+not be able to access the "n" variable after the method call
+*/
 
-/* Note: primitive datatypes: {integers, floats, booleans, tuples, and characters} all implement the "Copy" trait;
-meaning they can be passed as arguments directly without using "&" and their value data will be copied */
+/*
+Note: primitive datatypes: {integers, floats, booleans, tuples, arrays, and characters} all implement the "Copy" trait;
+meaning they can be passed as arguments directly without using "&" and their value data will be automatically copied
+*/
 
 /*
 *** BIG QUESTION ***
 Q.) When should I *move* ownership of a variable, "copy" a variable, or pass a *reference* to a variable?
 
-Ans. When dealing with simple datatypes (e.g., small "structs" or "enums"), moving their ownership by passing them directly
+Ans:
+    When dealing with simple datatypes (e.g., small "structs" or "enums"), moving their ownership by passing them directly (pass by value)
     is more efficient, because transferring their ownership is cheap and doesn't involve any copying of the data, especially
-    if the function/method needs to mutate the value.
+    if the function/method needs to mutate the value, and access to the variable is unneeded afterwards.
 
-    When dealing with primitive datatypes (that implement the "Copy" trait), copying the data is faster,
-    and there is no need to manage their ownership or borrowing by passing them as references using the "&".
+    When dealing with primitive datatypes that implement the "Copy" trait, you should pass their variables by value, because
+    copying their data is faster since they're trivially copyable, and there is no need to manage their ownership or
+    borrowing by passing them as references using the "&".
 
     When dealing with large or complex datatypes, it's more efficient to use a *reference* "&"; which essentially is a
-    pointer to the original value, because it avoids the cost of copying the data.
+    pointer to the original value, because it avoids the cost of copying (cloning) the data.
+*/
+
+/*
+Note: when using a reference "&", it's essentially a pointer to an address in memory, and when used in an x64 CPU architecture, it's size is 64 bits (8 bytes)!
  */
 
-/* Note: you CAN'T have a mutable reference and an immutable reference to a variable at the same time!
+/*
+Note: you CAN'T have a mutable reference and an immutable reference to a variable at the same time!
 Because one function/method can change the mutable reference to the variable while the other function/method expects it to remain the same
  */
